586d4590373f77a32819023695f3142972888a2d
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-11-18 17:06:04 +0530

    Add About component

---
+++ src/components/About.jsx
@@ -0,0 +1,47 @@
+const About = () => {
+	return (
+		<section id="about" className="cta-section pt100 pb50">
+			<div className="container" style={{ maxWidth: '1279px' }}>
+				<div className="p-lg-5 cta-content" style={{ flex: 'auto' }}>
+					<h2
+						className="sp-title"
+						style={{ fontSize: '42px', marginBottom: 0, textAlign: 'left' }}
+					>
+						About Us
+					</h2>
+					<div
+						style={{
+							height: '2px',
+							backgroundColor: '#e6e6e6',
+							marginBottom: '2%',
+						}}
+					></div>
+					<p>
+						The LNMIIT Students' Gymkhana isn't just a student government; it's
+						the heart of campus life. Established in 2006, our mission is to
+						empower students to develop their leadership skills, champion their
+						peers' interests, and foster a thriving community. We're more than
+						just a governing body. We're a platform for you to hone your
+						leadership skills, advocate for your peers, and build lifelong
+						connections. From leadership training to campus events, we offer a
+						vibrant space where you can truly thrive. Our student body elects a
+						Senate and Executive team, including a President and General
+						Secretaries for Cultural Council, Science & Tech Council, and Games
+						& Sports Council. Together, we work alongside the Institute's
+						Academic Senate and Board of Governors to ensure student voices are
+						heard in crucial decision-making processes. We also have nominated
+						representatives in departmental committees, ensuring both
+						undergraduate and postgraduate students have a say in shaping their
+						educational experience. But the Gymkhana isn't just about rules and
+						regulations. It's about creating an inclusive and empowering
+						environment where students can thrive. We encourage you to get
+						involved, share your ideas, and co-create a campus experience that's
+						truly unforgettable!
+					</p>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default About;
